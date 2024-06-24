95f215d86753d8f75b4650b7bdafffb813a6e70a
chris <davischris25139@yahoo.com>
2024-06-23 21:30:29 -0700

    installed dependancies for servers

--- vite-project/server/models/User.js
+++ vite-project/server/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcryptjs');
+// const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true },
@@ -7,13 +7,13 @@ const userSchema = new mongoose.Schema({
 });
 
 // Hash password before saving
-userSchema.pre('save', async function (next) {
-  if (this.isModified('pin') || this.isNew) {
-    const salt = await bcrypt.genSalt(10);
-    this.pin = await bcrypt.hash(this.pin, salt);
-  }
-  next();
-});
+// userSchema.pre('save', async function (next) {
+//   if (this.isModified('pin') || this.isNew) {
+//     const salt = await bcrypt.genSalt(10);
+//     this.pin = await bcrypt.hash(this.pin, salt);
+//   }
+//   next();
+// });
 
 const User = mongoose.model('User', userSchema);
 
