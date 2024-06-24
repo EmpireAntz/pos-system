79c945be98d8f84ba0176c91b684ee94c185e033
EmpireAntz <thebigboognish@gmail.com>
2024-06-23 19:53:32 -0700

    inital project

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+   <div>
+      <button>
+        Tap to login
+      </button>
+   </div>
+  )
+}
+
+export default App
